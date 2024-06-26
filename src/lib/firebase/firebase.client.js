6273eebba4733a7a933a7700e53b1d1a2be04924
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-26 22:01:49 +0700

    setup firebase database

---
+++ src/lib/firebase/firebase.client.js
@@ -0,0 +1,24 @@
+import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth, setPersistence, inMemoryPersistence } from "firebase/auth";
+
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_APIKEY,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_APPID,
+};
+
+// Initialize Firebase
+let firebaseApp;
+
+if (!getApps().length) {
+	firebaseApp = initializeApp(firebaseConfig);
+} else {
+	firebaseApp = getApp();
+	deleteApp(firebaseApp);
+	firebaseApp = initializeApp(firebaseConfig);
+}
+
+export const auth = getAuth(firebaseApp);
