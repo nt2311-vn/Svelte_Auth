53e034ee918a6981d683c1484a11b95c990e0a00
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-26 22:02:01 +0700

    layout svelte before viewing home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { onMount } from 'svelte';
+	import { auth } from '../lib/firebase/firebase.client.js';
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			console.log(user);
+		});
+	});
+</script>
+
+<slot />
